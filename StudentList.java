9b60a26429b268b3a81fde5dacf49e20470af113
Ambia-Ela <ambiaela7275@gmail.com>
2022-11-26 22:25:31 -0800

    task7 complited

--- StudentList.java
+++ StudentList.java
@@ -2,8 +2,7 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 public class StudentList {
-	static	Constants constants = new Constants();
-// get input from textfile 
+ static	Constants constants = new Constants();
 	public static BufferedReader getBufferedReader () {
 				try 
 				{
@@ -17,90 +16,47 @@ public class StudentList {
 				return null;
 	}
 	public static void main(String[] args) {
-		
-
-
-		if(args[0].equals(constants.inputfile1 )) {
-			if (args.length != 1)
-			{
-				System.out.println("please enter valid Argument");
-			}
-			System.out.println("Loading data ...");			
-			try {
-					BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(constants.inputfile))); 
-					String readString = bufferedReader.readLine();
-					String i[] = readString.split(",");			
-					for(String j : i)
-					 { 
-						System.out.println(j); 
-					 }
-				}
-			catch (Exception e){} 
-
-			   System.out.println("Data Loaded.");
-		}
-		else if(args[0].equals(inputfile2 ))
-		if (args.length != 1)
-		{
-			System.out.println("please enter valid Argument");
-		}
-		
-		 else if(args[0].contains(constants.inputfile3))
-
-		{
-			
-							
-				System.out.println("Data Loaded.");	
-		}
-		 if(args[0].contains(constants.inputfile4 )) 
-		{
-			System.out.println("Loading data ...");			
-			try {
-					BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt"))); 
-					String readString = bufferedReader.readLine();
-					String i[] = readString.split(",");	
-					boolean done = false;
-					String inpuString = args[0].substring(1);
-					for(int idx = 0; idx<i.length && !done; idx++) {
-					if(i[idx].equals(t)) {
-					System.out.println("We found it!");
-					done=true;
-				}
-			}
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");				
-		}
-		else if(args[0].contains(constants.inputfile4)) 
-		if (args.length != 1)
-		{
-			System.out.println("please enter valid Argument");
-		}
-		{
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt"))); 
-			String newdata = bufferedReader.readLine();
-			char a[] = newdata.toCharArray();			
-			boolean in_word = false;
-			int count=0;
-			for(char c:a)
-			 {
-
-				if(c ==' ') 
-				{
-					if (!in_word)
-					 {	count++; 
-						in_word =true;
+		// Check arguments
+		if (args[0].equals(constants.inputfile1)) {
+			if (args.length != 1) {
+				System.out.println("please enter valid arg");
+			} else {
+				System.out.println("Loading data ...");
+				try {
+					BufferedReader bufferedReader = getBufferedReader();
+					String reaString = bufferedReader.readLine();
+					String i[] = reaString.split(",");
+					for (String j : i) {
+						System.out.println(j);
 					}
-					else 
-					{ 
-						in_word=false;
-					}			
+				} catch (Exception e) {
 				}
+				System.out.println("Data Loaded.");
 			}
-			System.out.println(count +" word(s) found " + a.length);
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");				
+		} else if (args[0].equals( constants.inputfile2)) {
+			if (args.length != 1) {
+				System.out.println("valid arg");
+			} else {
+				System.out.println("Loading data ...");
+				try {
+					BufferedReader bufferedReader = getBufferedReader();
+					String readString = bufferedReader.readLine();
+					String i[] = readString.split(",");
+					int y = (int) ((double) Math.random() * (int) i.length);
+					System.out.println(i[y]);
+				} catch (Exception e) {
+				}
+				System.out.println("Data Loaded.");
+			}
+		} else if (args[0].contains(constants.inputfile3)) {
+			System.out.println("Loading data ...");
+			try {
+				BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("students.txt", true));
+				String inputString = args[0].substring(1);
+				Date newdDate = new Date();
+			
+				bufferedWriter.write(", " + inputString + "\nList last updated on " + dateFormat.format(newdDate));
+				bufferedWriter.close();
+			} catch (Exception e) 
 		}
-	}
-}
+		
