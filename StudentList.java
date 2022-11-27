b899d9a40d296827536062d6562b5e83aec6390d
Ambia-Ela <ambiaela7275@gmail.com>
2022-11-26 21:15:26 -0800

    task1 completed

--- StudentList.java
+++ StudentList.java
@@ -8,62 +8,62 @@ public class StudentList {
 		if(args[0].equals("a")) {
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");			
-			for(String j : i) { System.out.println(j); }
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");
+					BufferedReader s = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt"))); 
+					String r = s.readLine();
+					String i[] = r.split(",");			
+					for(String j : i)
+					 { 
+						System.out.println(j); 
+					 }
+				}
+			catch (Exception e){} 
+
+			   System.out.println("Data Loaded.");
 		}
 		else if(args[0].equals("r")) 
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			System.out.println(r);
-			String i[] = r.split(",");	
-			Random x = new Random();
-				int y = x.nextInt();
+					BufferedReader s = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt"))); 
+					String r = s.readLine();
+					System.out.println(r);
+					String i[] = r.split(",");	
+					Random x = new Random();
+					int y = x.nextInt();
 					System.out.println(i[y]);
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");			
+			    }
+				catch (Exception e){} 
+				System.out.println("Data Loaded.");			
 		}
-		else if(args[0].contains("+")){
-			System.out.println("Loading data ...");			
+		else if(args[0].contains("+"))
+		{
+				System.out.println("Loading data ...");			
 			try {
-			BufferedWriter s = new BufferedWriter(
-					new FileWriter("students.txt", true));
-			String t = args[0].substring(1);
-	        Date d = new Date();
-	        String df = "dd/mm/yyyy-hh:mm:ss a";
-	        DateFormat dateFormat = new SimpleDateFormat(df);
-	        String fd= dateFormat.format(d);
-			s.write(", "+t+"\nList last updated on "+fd);
-			s.close();
+					BufferedWriter s = new BufferedWriter(new FileWriter("students.txt", true));
+					String t = args[0].substring(1);
+					Date d = new Date();
+					String df = "dd/mm/yyyy-hh:mm:ss a";
+					DateFormat dateFormat = new SimpleDateFormat(df);
+					String fd= dateFormat.format(d);
+					s.write(", "+t+"\nList last updated on "+fd);
+					s.close();
 			} catch (Exception e){}
 							
-			System.out.println("Data Loaded.");	
+				System.out.println("Data Loaded.");	
 		}
 		else if(args[0].contains("?")) 
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");	
-			boolean done = false;
-			String t = args[0].substring(1);
-			for(int idx = 0; idx<i.length && !done; idx++) {
-				if(i[idx].equals(t)) {
+					BufferedReader s = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt"))); 
+					String r = s.readLine();
+					String i[] = r.split(",");	
+					boolean done = false;
+					String t = args[0].substring(1);
+					for(int idx = 0; idx<i.length && !done; idx++) {
+					if(i[idx].equals(t)) {
 					System.out.println("We found it!");
-						done=true;
+					done=true;
 				}
 			}
 			} catch (Exception e){} 
@@ -73,14 +73,14 @@ public class StudentList {
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+			BufferedReader s = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt"))); 
 			String D = s.readLine();
 			char a[] = D.toCharArray();			
 			boolean in_word = false;
 			int count=0;
-			for(char c:a) {
+			for(char c:a)
+			 {
+
 				if(c ==' ') 
 				{
 					if (!in_word) {	count++; in_word =true;	}
